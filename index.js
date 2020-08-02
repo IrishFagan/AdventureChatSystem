1fcdd1a58a6543cf15558c5c67d4774c8dc038c3
Irish Fagan <IrishFagan@live.com>
2020-08-01 17:04:28 -0700

    Add code to start server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const socket = require('socket.io')
+
+const app = express()
+const port = 8080
+
+var server = app.listen(port, function() {
+	console.log(`Listening on port ${port}`)
+})
