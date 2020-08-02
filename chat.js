91cba391b8e84806deb4f7f0a6f33e393270f763
Irish Fagan <IrishFagan@live.com>
2020-08-02 12:09:25 -0700

    Add functionality for appending data from client to chat window. Remove default event for the chat form submission. Revise form submission to call sendMessage function

--- chat.js
+++ chat.js
@@ -1,13 +1,23 @@
-var socket = io.connect('ws://localhost:8080')
+var socket = io.connect('ws://localhost:8080');
 
-var sendChat = document.getElementById("message-send")
-var message = document.getElementById("message-box")
+var sendChat = document.getElementById("message-send");
+var message = document.getElementById("message-box");
 
-sendChat.addEventListener('click', function() {
-	alert("sending chat: " + message.value)
-	socket.emit('chat', message.value)
+function sendMessage() {
+	alert("sending chat: " + message.value);
+	socket.emit('chat', message.value);
+}
+
+sendChat.addEventListener('click', function () {
+	sendMessage();
+})
+
+$("#chat-form").submit(function (evt) {
+	evt.preventDefault();
+	sendMessage();
 })
 
 socket.on('chat', function(data) {
-	console.log(data)
+	$("#chat-window").append('<p>'+data+'</p>');
+	console.log(data);
 })
