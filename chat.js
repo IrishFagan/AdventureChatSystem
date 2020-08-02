45563d63a808c7d7a4db1f85cd09532e2c8dc280
Irish Fagan <IrishFagan@live.com>
2020-08-01 18:20:00 -0700

    Add event listener for chat button. Add functionality to emit a message

--- chat.js
+++ chat.js
@@ -2,3 +2,8 @@ var socket = io.connect('ws://localhost:8080')
 
 var sendChat = document.getElementById("send")
 var message = document.getElementById("message")
+
+sendChat.addEventListener('click', function() {
+	alert("sending chat: " + message.value)
+	socket.emit('chat', message.value)
+})
