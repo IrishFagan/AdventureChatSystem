6622d4c5333f016ea75914b49323cb13ff28f092
Irish Fagan <IrishFagan@live.com>
2020-08-02 22:06:26 -0700

    Implement functionality to have a username

--- chat.js
+++ chat.js
@@ -3,24 +3,26 @@ var socket = io.connect('ws://localhost:8080');
 var sendChat = document.getElementById("message-send");
 var message = document.getElementById("message-box");
 
+var username = "Iro";
+
 function sendMessage() {
 	if(message.value != "") {
-		socket.emit('chat', message.value);
+		socket.emit('chat', [username, message.value]);
 	}
 }
 
 sendChat.addEventListener('click', function () {
 	sendMessage();
-})
+});
 
 $("#chat-form").submit(function (evt) {
 	evt.preventDefault();
 	sendMessage();
-})
+});
 
 socket.on('chat', function(data) {
 	$("#chat-window").append('<p class="message">'+data+'</p>');
 	$("#message-box").val("");
-	$("#chat-window").scrollTop($("#chat-window")[0].scrollHeight)
+	$("#chat-window").scrollTop($("#chat-window")[0].scrollHeight);
 	console.log(data);
-})
+});
